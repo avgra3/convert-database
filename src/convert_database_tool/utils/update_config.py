48566b2438f2dbd3e07e6103f1200b9d3393bb24
avgra3 <antonygradillas@gmail.com>
2025-11-25 12:29:17 -0700

    update import mismatch

--- src/convert_database_tool/utils/update_config.py
+++ src/convert_database_tool/utils/update_config.py
@@ -1,4 +1,4 @@
-from convert_database_tool.utils.constants import db_config_toml, LOGGER, db_config_file
+from convert_database_tool.utils.constants import db_config_toml, LOGGER, get_config_file
 from tomlkit import dumps, document
 
 
@@ -9,7 +9,7 @@ def update_field(field_name: str, value: str | int) -> None:
         LOGGER.warning(f"`{field_name.strip().lower()}` is not a valid field to update.")
         return
     current_config[field_name.strip().lower()] = value
-    config_file = db_config_file()
+    config_file = get_config_file("dbConfig.toml")
     doc = document()
     doc.update({"database_config": current_config})
     with open(config_file, "w", encoding="utf-8") as f:
