4909fab7179f1665f891d331937c054d259a18de
avgra3 <antonygradillas@gmail.com>
2026-09-08 18:56:43 -0500

    update to parsing

--- src/convert_database_tool/utils/query.py
+++ src/convert_database_tool/utils/query.py
@@ -9,14 +9,14 @@ class Conversion:
         self.log = self.dbPool.log
 
     def getTablesToConvert(self, script: str) -> list[str]:
-        query_result: NeilResult = self.dbPool.execute_sql(
-            sql=script
-        )
+        query_result: NeilResult = self.dbPool.execute_sql(sql=script)
         for err in query_result.errors:
             self.dbPool.log.critical(err)
         if query_result.returnedData is None:
-            return
-        results: list[str] = [sql for sql in query_result.returnedData]
+            return []
+        results: list[str] = [
+            "".join((str(s) for s in sql)) for sql in query_result.returnedData
+        ]
         return results
 
     def run_queries(self, queries: list[str]) -> None:
@@ -28,4 +28,10 @@ class Conversion:
         if tables_to_convert is None or len(tables_to_convert) == 0:
             self.dbPool.log.warning("no data returned")
             return
-        self.run_queries(queries=[sql for sql in tables_to_convert if sql.strip() != "" and sql.strip() !=";"])
+        self.run_queries(
+            queries=[
+                sql
+                for sql in tables_to_convert
+                if sql.strip() != "" and sql.strip() != ";"
+            ]
+        )
